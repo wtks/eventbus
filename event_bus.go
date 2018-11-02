ae5ac6c01524ffb46e69145cba15564fbf83ac81
takashi <t.k.mk3.110@gmail.com>
2018-11-02 11:33:31 +0900

    support glob subscribe

--- event_bus.go
+++ event_bus.go
@@ -1,6 +1,7 @@
 package eventbus
 
 import (
+	"github.com/gobwas/glob"
 	"reflect"
 	"sync"
 	"time"
@@ -10,23 +11,34 @@ type Handler func(event string, time time.Time, args ...interface{})
 
 type EventBus struct {
 	handlers map[string][]Handler
+	compiled map[string]glob.Glob
+	cached   map[string][]Handler
 	m        sync.RWMutex
 	wg       sync.WaitGroup
+	cacheM   sync.RWMutex
 }
 
 func New() *EventBus {
 	return &EventBus{
 		handlers: make(map[string][]Handler),
-		m:        sync.RWMutex{},
-		wg:       sync.WaitGroup{},
+		compiled: make(map[string]glob.Glob),
+		cached:   make(map[string][]Handler),
 	}
 }
 
-func (bus *EventBus) Subscribe(event string, fn Handler) {
+func (bus *EventBus) Subscribe(event string, fn Handler) error {
 	bus.m.Lock()
 	defer bus.m.Unlock()
-
+	if _, ok := bus.compiled[event]; !ok {
+		g, err := glob.Compile(event, ':')
+		if err != nil {
+			return err
+		}
+		bus.compiled[event] = g
+	}
 	bus.handlers[event] = append(bus.handlers[event], fn)
+	bus.forgetCache()
+	return nil
 }
 
 func (bus *EventBus) Unsubscribe(event string, fn Handler) bool {
@@ -45,6 +57,7 @@ func (bus *EventBus) Unsubscribe(event string, fn Handler) bool {
 			}
 		}
 	}
+	bus.forgetCache()
 	return false
 }
 
@@ -53,6 +66,7 @@ func (bus *EventBus) UnsubscribeAll(event string) {
 	defer bus.m.Unlock()
 
 	bus.handlers[event] = make([]Handler, 0)
+	bus.forgetCache()
 }
 
 func (bus *EventBus) Publish(event string, args ...interface{}) {
@@ -60,7 +74,7 @@ func (bus *EventBus) Publish(event string, args ...interface{}) {
 	defer bus.m.RUnlock()
 
 	now := time.Now()
-	if hs, ok := bus.handlers[event]; ok && len(hs) > 0 {
+	if hs := bus.findHandlers(event); len(hs) > 0 {
 		for _, h := range hs {
 			bus.wg.Add(1)
 			go bus.publish(h, event, now, args...)
@@ -75,4 +89,31 @@ func (bus *EventBus) publish(fn Handler, event string, time time.Time, args ...i
 
 func (bus *EventBus) WaitAll() {
 	bus.wg.Wait()
+}
+
+func (bus *EventBus) findHandlers(topic string) []Handler {
+	bus.cacheM.RLock()
+	if hs, ok := bus.cached[topic]; ok {
+		bus.cacheM.RUnlock()
+		return hs
+	}
+	bus.cacheM.RUnlock()
+
+	var result []Handler
+	for k, matcher := range bus.compiled {
+		if matcher.Match(topic) {
+			result = append(result, bus.handlers[k]...)
+		}
+	}
+
+	bus.cacheM.Lock()
+	bus.cached[topic] = result
+	bus.cacheM.Unlock()
+	return result
+}
+
+func (bus *EventBus) forgetCache() {
+	bus.cacheM.Lock()
+	bus.cached = map[string][]Handler{}
+	bus.cacheM.Unlock()
 }
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/wtks/eventbus
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gobwas/glob v0.2.3
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.1.1 // indirect
+	github.com/stretchr/testify v1.2.2
+)
